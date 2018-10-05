d867c31107fc4789a439dbed2a9058a40cd42820
Renan Santos <i866982@slel00681779a.amer.global.corp.sap>
2018-10-04 21:23:19 -0300

    Deixando o server menos merda

--- TrabGA_SERVER/main.c
+++ TrabGA_SERVER/main.c
@@ -21,6 +21,66 @@
 
 #define MAXBUFSIZE 1000 // O tamanho max. do pacote UDP Packet é 64 Kbytes (65535 bytes, que é o valor máximo representado pelos dois bytes destinados ao tamanho do datagrama UDP).
 
+int sock, status, socklen;
+unsigned char buffer[MAXBUFSIZE];
+unsigned char rng[MAXBUFSIZE];
+struct sockaddr_in saddr;
+
+void fechaSocket();
+
+void preencheBuffersComZeros();
+
+void iniciaServidorUDP();
+
+void medeUmidade();
+
+void medeTemperatura();
+
+void medeVolume();
+
+int temperatura;
+int volume;
+int umidade;
+
+bool verificaCodigoDeIdentificacao(char bufferDeVerificacao[]) {
+    if(bufferDeVerificacao[0] == '1') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void configuraSocketUDP() {
+    memset(&saddr, 0, sizeof(struct sockaddr_in));
+
+    // Abre um socket UDP
+    sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (sock < 0) {
+        perror("Erro ao criar socket");
+        exit(0);
+    }
+
+    saddr.sin_family = PF_INET;
+    saddr.sin_port = htons(38000); // Escuta na porta 38000. (big endian x little endian)
+
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    status = bind(sock, (struct sockaddr *) &saddr, sizeof(struct sockaddr_in));
+
+    if (status < 0) {
+        perror("Erro no binding do socket em uma interface");
+        exit(0);
+    }
+
+    socklen = sizeof(struct sockaddr_in);
+
+    system("clear");
+    printf("\t##### Servidor UDP #####\n");
+    printf("\tEsperando por chamadas...\n");
+
+    printf("\tport %d...\n", htons(saddr.sin_port));
+}
+
 /*
 ** reverse string in place
 */
@@ -54,62 +114,45 @@ void itoa(int n, char s[]) {
 
 //Descontados os cabeçalhos do UDP e do IP temos 65507 bytes.
 int main() {
-    int sock, status, socklen;
-    unsigned char buffer[MAXBUFSIZE];
-    unsigned char rng[MAXBUFSIZE];
-    struct sockaddr_in saddr;
 
-    memset(&saddr, 0, sizeof(struct sockaddr_in));
+    configuraSocketUDP();
 
-    // Abre um socket UDP
-    sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
-    if (sock < 0) {
-        perror("Error creating socket");
-        exit(0);
-    }
+    iniciaServidorUDP();
 
-    saddr.sin_family = PF_INET;
-    saddr.sin_port = htons(38000); // Escuta na porta 38000. (big endian x little endian)
+    fechaSocket();
 
-    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    status = bind(sock, (struct sockaddr *) &saddr, sizeof(struct sockaddr_in));
+    return 0;
+}
 
-    if (status < 0) {
-        perror("Error binding socket to interface");
-        exit(0);
-    }
-
-    socklen = sizeof(struct sockaddr_in);
-
-    system("clear");
-    printf("\t##### UDP server #####\n");
-    printf("\tWaiting for requests...\n");
-
-    printf("\tport %d...\n", htons(saddr.sin_port));
-
-    int temperatura;
-
+void iniciaServidorUDP() {
     while (1) {
         srand(time(0));
-        temperatura = 1 + rand() % 9;
+
+        medeTemperatura();
+        medeUmidade();
+        medeVolume();
 
         printf("\tTemperatura: %i\n", temperatura);
+
+        printf("\tUmidade: %i\n", umidade);
+
+        printf("\tVolume: %i\n", volume);
+
 
         // Recebe dados do socket
         status = recvfrom(sock, buffer, MAXBUFSIZE, 0,
                           (struct sockaddr *) &saddr, &socklen);
 
         // Envia resposta
-        printf("\tReceived data: %s From: %s, port: %d\n\n", buffer, inet_ntoa(saddr.sin_addr), htons(saddr.sin_port));
+        printf("\tDado recebido: %s De: %s, port: %d\n\n", buffer, inet_ntoa(saddr.sin_addr), htons(saddr.sin_port));
 
         // Verifica se o primeiro byte do buffer é igual a 1
-//        if (!strcmp(buffer, "1")) {
-        if(buffer[0] == '1'){
+        if(verificaCodigoDeIdentificacao(buffer)) {
             printf("\tEntrou aqui\n");
-            printf("\tSending response...\n");
+            printf("\tEnviando resposta...\n");
 
 
-            itoa(temperatura, &rng);
+            itoa(temperatura, rng);
             strcat(buffer, rng);
 
             status = 0;
@@ -120,9 +163,7 @@ int main() {
             printf("\tTem uma mensagem de erro genérica aqui\n");
 
 
-//            strcpy(buffer, "Hello, Im am the server.\n");
-
-            memset(&buffer, 0, sizeof(buffer));
+            preencheBuffersComZeros();
 
             strcat(buffer, "255");
 
@@ -132,21 +173,22 @@ int main() {
         }
 
         // Preeenche o buffer com zeros
-        memset(&buffer, 0, sizeof(buffer));
-        memset(&buffer, 0, sizeof(rng));
-
-        // Escreve msg no buffer
-//        strcpy(buffer, "Hello, Im am the server.\n");
-
-        // Envia
-//        status=0;
-
-//        printf("\tSending response...\n");
-//        status = sendto(sock, buffer, strlen(buffer), 0,
-//                        (struct sockaddr *)&saddr, socklen);
+        preencheBuffersComZeros();
     }
+}
+
+void medeVolume() { volume = 1 + rand() % 9; }
+
+void medeTemperatura() { temperatura = 1 + rand() % 50; }
+
+void medeUmidade() { umidade = 1 + rand() % 100; }
+
+void preencheBuffersComZeros() {
+    memset(&buffer, 0, sizeof(buffer));
+    memset(&buffer, 0, sizeof(rng));
+}
+
+void fechaSocket() {
     shutdown(sock, 2);
     close(sock);
-
-    return 0;
 }
